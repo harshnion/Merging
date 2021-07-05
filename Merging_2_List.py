213de853904969803aa5a7bbd063ee452eddd5d1
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-05 23:34:36 +0530

    11_commit

---
+++ Merging_2_List.py
@@ -0,0 +1,13 @@
+# So hemlo Guys 
+# Shuru karte h binna kisi ......... Ke 
+
+# Today we learn about merging 2 list with the help of zip()
+# SO here is our 2 list which we need to merge
+A = ["apple","banana","Grapes"]
+B = [1,2,3]
+
+# Here we learn how to use zip in our code 
+orders = zip(A,B)
+# after everytime we use zip we need to tell the type of output 
+print(list(orders))
+
